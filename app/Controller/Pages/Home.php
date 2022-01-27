b98f8cd3ee4a06663f34c81782367393982708f4
felipertec <felipe.rtec@gmail.com>
2022-01-27 14:30:57 -0300

    Primeiro Commit
    Esse é o primeiro commit do projeto.

---
+++ app/Controller/Pages/Home.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Pages;
+
+use App\Utils\View;
+use App\Model\Entity\Organization;
+
+class Home extends Page{
+
+    public static function getHome(){
+
+        $obOrganization = new Organization;
+
+        $content = View::render('pages/home', [
+            'name' => $obOrganization->name,
+            'description' => $obOrganization->description
+        ]);
+        return parent::getPage('Primeiro MVC em PHP', $content);
+    }
+
+}
